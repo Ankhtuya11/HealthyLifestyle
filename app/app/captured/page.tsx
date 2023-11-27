e8970e1e8e255c937b6b08d19a4d5ce4ee525a53
Anhaa <sw21d029@mandakh.edu.mn>
2023-11-27 15:15:59 +0800

    loading

--- app/app/captured/page.tsx
+++ app/app/captured/page.tsx
@@ -1,68 +1,82 @@
-
 "use client"
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { useSearchParams } from 'next/navigation';
 import axios from "axios";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import Lottie from "lottie-react";
+import animationData from "@/public/assets/lottie/gradient_orb.json";
+
 export default function Page() {
   const searchParams = useSearchParams();
+  const txtData2 = searchParams.get("data") || "";
   const [translated, setTranslated] = useState("");
-  const txtData2 = searchParams.get("data") ?? "";
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(true); // Set loading to true initially
+  const [error, setError] = useState(null);
+
+  const fetchTranslation = useCallback(async () => {
+    try {
+      setError(null);
+
+      const response = await axios.post('https://google-translate113.p.rapidapi.com/api/v1/translator/text', {
+        from: 'auto',
+        to: 'en',
+        text: txtData2,
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-RapidAPI-Key': 'c0a2d3b7a8mshe348abd873a4e4ep17385djsn44c42053c9a5',
+          'X-RapidAPI-Host': 'google-translate113.p.rapidapi.com',
+        },
+      });
+
+      setTranslated(prevState => response.data.trans);
+    } catch (error) {
+    } 
+  }, [txtData2]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      
-      
-      const encodedParams = new URLSearchParams();
-      encodedParams.set('from', 'auto');
-      encodedParams.set('to', 'en');
-      encodedParams.set('text', txtData2);
+    if (txtData2) {
+      fetchTranslation();
+    }
+  }, [txtData2, fetchTranslation]);
 
-      const options = {
-        method: 'POST',
-        url: 'https://google-translate113.p.rapidapi.com/api/v1/translator/text',
-        headers: {
-          'content-type': 'application/x-www-form-urlencoded',
-          'X-RapidAPI-Key': 'c0a2d3b7a8mshe348abd873a4e4ep17385djsn44c42053c9a5',
-          'X-RapidAPI-Host': 'google-translate113.p.rapidapi.com'
-        },
-        data: encodedParams,
+  useEffect(() => {
+    if (translated) {
+      const fetchGPT = async () => {
+        try {
+          const response = await axios.post('https://chatgpt-openai1.p.rapidapi.com/ask', {
+            query: `${translated} give me only pros and cons of this food`,
+          }, {
+            headers: {
+              'Content-Type': 'application/json',
+              'X-RapidAPI-Key': '2fafd55848mshe2b9996f8085a32p1f512ajsnc4a107eefd2c',
+              'X-RapidAPI-Host': 'chatgpt-openai1.p.rapidapi.com',
+            },
+          });
+
+          console.log(response.data.response);
+          setResult(response.data.response);
+          // Handle the response as needed
+        } catch (error) {
+        }finally {
+          setLoading(false); // Set loading to false regardless of success or failure
+        }
       };
 
-      try {
-        const response = await axios.request(options);
-        console.log(response.data);
-        setTranslated(response.data.trans)
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData(); // Call the async function
-  }, [searchParams]);
+      fetchGPT();
+    }
+  }, [translated]);
 
   return (
     <div>
-      {translated}
-      {translated ? (
-         <Link href={{
-          pathname: "/result",
-          query:{
-            data: translated,
-          },
-         }}>go to resuklt</Link>
-        ) : (
-          <p>asda</p>
-        )}
+      {loading && (
+        <div className="flex justify-center items-center">
+          <Lottie animationData={animationData} loop={true} />
+        </div>
+      )}
+      {result && <p>{result}</p>}
+      {error && <p className="text-red-500">{error}</p>}
     </div>
-    
   );
 }
-
-
-
-
-// https://rapidapi.com/undergroundapi-undergroundapi-default/api/google-translate113
-
-
