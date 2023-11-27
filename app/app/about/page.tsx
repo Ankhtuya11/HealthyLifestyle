6e261e69ebc9d40dd65998407478879b52cebf7c
Shirendev08 <shirendev.07@gmail.com>
2023-11-27 17:01:02 +0800

    about inside

--- app/app/about/page.tsx
+++ app/app/about/page.tsx
@@ -1,8 +1,122 @@
 "use client"
 import React from 'react'
-import Header from '@/components/header'
-export default function page() {
+import mandakh from '../../public/assets/mandakh.jpg';
+import Image from 'next/image'; 
+export default function page() { 
+ 
   return (
-    <div>juf</div>
+    <>
+ {/* <div  className='w-full text-white my-24 '>
+       
+        </div>
+        <div className='max-w-[1240px] mx-auto '>
+
+
+
+<div className="mt-12 grid grid-cols-3 gap-2">
+    <div className="h-12  ">
+    <div className=' '>
+         
+         </div>
+         
+         <div className='max-w-[1240px] mx-auto '>
+             <div className='text-center py-8'>
+             </div>
+  
+ 
+             <div className='grid'>
+                 
+                 <div className='bg-blue text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative'>
+           
+                     <div className='flex grid grid-cols-1 lg:grid-cols-2 relative'>
+                   
+                        
+                     </div>
+                     <p className='text-1xl  sm:text-3xl text-indigo-600 center text-center font-semibold '>Мандах Их Сургууль</p>
+                     <p className='text-1xl sm:text-2xl  text-indigo-700 center text-center'>Мэдээлэл, Технологийн Сургууль</p>
+                     <div className='text-2xl '>
+                         <p className='flex py-3 text-sm sm:text-xl'>Мандах Их Сургуулийн Мэдээлэл, Технологийн Сургуулийн багш, оюутнуудын хамтын бүтээл юм. Нэг зорилготойгоор нэгдэн нийлсэн шинэ шинэлэг технологийг эрэлхийлэгч залуус тус аппликейшнийг үнэ төлбөргүйгээр танд хүргэж байна.</p>
+                        
+                         
+                     </div>
+                 </div>
+                 
+             </div>
+ 
+         </div>
+    </div>
+    <div className="h-12 ">
+    <div className=' '>
+         
+         </div>
+         
+         <div className='max-w-[1240px] mx-auto '>
+             <div className='text-center py-8'>
+             </div>
+  
+ 
+             <div className='grid'>
+                 
+                 <div className='bg-blue text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative'>
+           
+                     <div className='flex grid grid-cols-1 lg:grid-cols-2 relative'>
+                   
+                     </div>
+                     <p className='text-1xl  sm:text-3xl text-indigo-600 center text-center font-semibold '>Мандах Их Сургууль</p>
+                     <p className='text-1xl sm:text-2xl  text-indigo-700 center text-center'>Мэдээлэл, Технологийн Сургууль</p>
+                     <div className='text-2xl '>
+                         <p className='flex py-3 text-sm sm:text-xl'>Мандах Их Сургуулийн Мэдээлэл, Технологийн Сургуулийн багш, оюутнуудын хамтын бүтээл юм. Нэг зорилготойгоор нэгдэн нийлсэн шинэ шинэлэг технологийг эрэлхийлэгч залуус тус аппликейшнийг үнэ төлбөргүйгээр танд хүргэж байна.</p>
+                        
+                         
+                     </div>
+                 </div>
+                 
+             </div>
+ 
+         </div>
+    </div>
+    <div className="h-12 ">
+    <div className=' '>
+         
+         </div>
+         
+         <div className='max-w-[1240px] mx-auto '>
+             <div className='text-center py-8'>
+             </div>
+  
+ 
+             <div className='grid'>
+                 
+                 <div className='bg-blue text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative'>
+           
+                     <div className='flex grid grid-cols-1 lg:grid-cols-2 relative'>
+                   
+                        
+                     </div>
+                     <p className='text-1xl  sm:text-3xl text-indigo-600 center text-center font-semibold '>Мандах Их Сургууль</p>
+                     <p className='text-1xl sm:text-2xl  text-indigo-700 center text-center'>Мэдээлэл, Технологийн Сургууль</p>
+                     <div className='text-2xl '>
+                         <p className='flex py-3 text-sm sm:text-xl'>Мандах Их Сургуулийн Мэдээлэл, Технологийн Сургуулийн багш, оюутнуудын хамтын бүтээл юм. Нэг зорилготойгоор нэгдэн нийлсэн шинэ шинэлэг технологийг эрэлхийлэгч залуус тус аппликейшнийг үнэ төлбөргүйгээр танд хүргэж байна.</p>
+                        
+                         
+                     </div>
+                 </div>
+                 
+             </div>
+ 
+         </div>
+    </div>
+</div>
+    </div> */}
+    <div className="grid grid-cols-3 gap-4 m-10">
+  <div className="...">01</div>
+  <div className="...">02</div>
+  <div className="...">03</div>
+  <div className="col-span-2 ...">04</div>
+  <div className="...">05</div>
+  <div className="...">06</div>
+  <div className="col-span-2 ...">07</div>
+</div>
+    </>
   )
 }
